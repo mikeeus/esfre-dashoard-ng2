345be173e0b6b641d1da8d472ccbdf82db48c096
Mikias <itsmikias@gmail.com>
2016-08-11 06:30:42 +0300

    Check that aut0 works then disable

--- ang-client/src/app/app.component.ts
+++ ang-client/src/app/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'root',
-  templateUrl: './root.html',
+  templateUrl: './app/root.html',
 })
 export class AppComponent {
 }
--- ang-client/src/home/home.component.ts
+++ ang-client/src/home/home.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'home',
-  templateUrl: '/app/home.component.html'
+  templateUrl: '/home/home.component.html'
 })
 export class HomeComponent {
   
